ad5446de38669d90d60d6e118f074c5215ec7ff7
SpeedyZ500 <92234505+SpeedyZ500@users.noreply.github.com>
2025-02-28 00:02:02 -0700

    forgot some things

--- src/stories/storypage/chapter/chapter.jsx
+++ src/stories/storypage/chapter/chapter.jsx
@@ -37,17 +37,29 @@ export function Chapter() {
     })
     return (
     <main className="chapter">
-        <h1 id="title" className="theme-h adaptive">Chapter Title</h1>
-        <table className="theme-h adaptive">
-            <tr>
-                <th>Genres:</th>
-                <td>Genre List here</td>
-            </tr>
-            <tr>
-                <th>Content Warnings:</th>
-                <td>Content Warnings List here</td>
-            </tr>
-        </table>
+        <h1 id="title" className="theme-h adaptive">{chapter.Name}</h1>
+        <table>
+                    <tbody>
+        
+                        {Array.isArray(story.info) && story.info.map((entry, index) => (
+                            <tr key={index}>
+                                <th>{entry.label}</th>
+                                <td>
+                                    <Fragment>
+                                        {entry.value.map((item,subIndex) =>(
+                                            <Fragment key={subIndex}>
+                                                
+                                                <span>{item}</span>
+                                                
+                                                {subIndex < entry.value.length - 1 && <span>, </span>}
+                                            </Fragment>
+                                        ))}
+                                    </Fragment>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
         <div className="buttons border-bottom" width="80%">
             <NavLink className="btn btn-primary" >Previous</NavLink>
             <NavLink className="btn btn-primary">Back to Story Page</NavLink>
@@ -58,9 +70,7 @@ export function Chapter() {
 
         <div className="textbody">
             <p>
-                Behold the body of the chapter, here is where you would read the chapter, if there was a chapter that existed
-                Yes I know that I will be needing to change the next button to like a dropdown, but this is just an example for now.
-                I'll make it so that you can select what branch you want to go down. Also think about adding a new chapter/branch button
+                {chapter.content}
             </p>
         </div>
         <div className="buttons border-top" width="80%">
