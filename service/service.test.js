83f067aae6d23ea03a53b6a224945744f611cebe
SpeedyZ500 <92234505+SpeedyZ500@users.noreply.github.com>
2025-03-19 18:55:59 -0600

    theoretically have a working server facade, I know I know, I should have been testing as I went, and should be commiting more often, Old habbits Die Hard

---
+++ service/service.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('./service');
+
+
+function getRandomName(prefix) {
+    return `${prefix}_${Math.random().toString(36).substring(2, 15)}`;
+}
+  
+async function registerUser() {
+    const email = getRandomName('email');
+    const username = getRandomName('username');
+    const password = 'toomanysecrets';
+    const response = await request(app).post('/api/auth/register').send({ email, username, password });
+
+    return [response, email, username, password];
+}
+
+test('register', async () => {
+    const [register, email, username] = await registerUser();
+    const displayname = username;
+    const profanityFilter = true;
+    expect(register.headers['content-type']).toMatch('application/json; charset=utf-8');
+    expect(register.body).toMatchObject({ email, username, displayname, profanityFilter });
+});
+
